3b1cbb8bd3c02179d4addf9654af2859584e324e
Brenda Velasco <al13020238@itsa.edu.mx>
2019-07-12 17:23:00 -0500

    head
    proyect1

--- harvest.R
+++ harvest.R
@@ -1,2 +1,11 @@
 library(bibliometrix)
+library(igraph)
+library(tidyverse)
+library(roadoi) # titles
+library(fulltext) # Abstract
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(cluster)
 D <- readFiles("/cloud/project/EM.txt")
+
